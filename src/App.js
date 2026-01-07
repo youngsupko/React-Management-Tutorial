972448349f74d6bbae01440ac4c0a0b86132bf7b
youngsupko <ysko91@naver.com>
2026-01-07 10:03:04 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers = [{
+  'id': 1,
+  'image': 'https://placehold.co/64/64/red',
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placehold.co/64/64/blue',
+  'name': '문재인',
+  'birthday': '201213',
+  'gender': '남자',
+  'job': '대통령'
+},
+{
+  'id': 3,
+  'image': 'https://placehold.co/64/64/black',
+  'name': '이정재',
+  'birthday': '991230',
+  'gender': '남자',
+  'job': '배우'
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key = {c.id}
+              id = {c.id}
+              image = {c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+            />
+          );
+        })
+      }
     </div>
+    
   );
 }
 
